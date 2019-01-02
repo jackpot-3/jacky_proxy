f5013ebc24b6a1baca2413e966ae7d201efb2e23
Jessica <jessicarhodson@gmail.com>
2019-01-02 13:29:49 -0800

    Make proxy responsive to path with id parameter

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
 app.use(cors());
+app.use('/:id', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 // app.use(bodyParser.text())
 app.listen(port, () => {
   console.log(`Server running on ${port}`)
