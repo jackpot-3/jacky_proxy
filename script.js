f7c6bf239c7ddba81db49ad28c018742575b2994
chujacky <jacky9407@gmail.com>
2019-01-25 16:20:34 -0800

    Install newrelic

---
+++ script.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export let options = {
+  vus: 200,
+  duration: "300s",
+  rps: 1000
+};
+
+export default function() {
+  const queryStrings = ['car', 'bacon', 'cotton', 'awesome', 'bike', 'intelligent', 'concrete', 'fresh', 'tasty', 
+  'metal', 'wooden', 'unbranded', 'practical', 'handmade', 'Tasty', 'Steel', 'Ergonomic', 'Gorgeous', 'computer', 
+  'gloves', 'Granite', 'Table', 'Salad', 'Towels', 'Shoes', 'small', 'refined'];
+
+  const query = queryStrings[Math.floor(Math.random() * queryStrings.length)];
+  http.get(`http://localhost:3003/products/all/${query}`);
+};
